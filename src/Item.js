4df1f7372beaf431d723d0c5521b68e492ee5fa9
Pedro La Rosa <pedrolarosad@gmail.com>
2020-12-19 17:06:54 +0100

    testing a regular item

--- src/Item.js
+++ src/Item.js
@@ -1,4 +1,10 @@
 export default class Item {
+  /**
+   * 
+   * @param {string} name 
+   * @param {number} sellIn 
+   * @param {number} quality 
+   */
   constructor (name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
--- src/Shop.test.js
+++ src/Shop.test.js
@@ -1,11 +1,39 @@
 import Shop from "./Shop";
 import Item from "./Item";
 
-test("canary test", () => {
-  const item = new Item("foo", 1, 3);
-  const shop = new Shop([item]);
+describe("Testing Shop class", () => {
+  describe("Testing a regular item", () => {
+    const regularItemName = "regular item";
+    it("should degrade the quality of the product by each day that it passes", () => {
+      const item = new Item(regularItemName, 1, 3);
 
-  shop.updateQuality();
+      new Shop([item]).updateQuality();
 
-  expect(item.quality).toEqual(2);
-});
+      expect(item).toEqual(new Item(regularItemName, 0, 2));
+    });
+
+    it("should not degrade the quality below to 0 when the item already has quality 0", () => {
+      const item = new Item(regularItemName, 1, 0);
+
+      new Shop([item]).updateQuality();
+
+      expect(item).toEqual(new Item(regularItemName, 0, 0));
+    });
+
+    it("should quality decrease twice as fast when the sellIn has passed", () => {
+      const item = new Item(regularItemName, 0, 5);
+
+      new Shop([item]).updateQuality();
+
+      expect(item).toEqual(new Item(regularItemName, -1, 3));
+    });
+
+    it("should not degrade the quality below 0 once the sell by date has passed", () => {
+      const item = new Item(regularItemName, 0, 0);
+
+      new Shop([item]).updateQuality();
+
+      expect(item).toEqual(new Item(regularItemName, -1, 0));
+    });
+  });
+})
