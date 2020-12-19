079b4d6eab0a5e71eaeb96f5e462c2d4e2f71ec4
Pedro La Rosa <pedrolarosad@gmail.com>
2020-12-19 17:42:59 +0100

    self encapsulate fields in Item class

--- src/Item.js
+++ src/Item.js
@@ -6,8 +6,17 @@ export default class Item {
    * @param {number} quality 
    */
   constructor (name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
+    this._name = name;
+    this._sellIn = sellIn;
+    this._quality = quality;
   }
+
+  get name() { return this._name; }
+  set name(value) { this._name = value; }
+
+  get sellIn() { return this._sellIn; }
+  set sellIn(value) { this._sellIn = value; }
+
+  get quality() { return this._quality; }
+  set quality(value) { this._quality = value; }
 }
