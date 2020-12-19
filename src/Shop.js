0b020c25146fa2861a7cfff3ff8385cce1408540
Pedro La Rosa <pedrolarosad@gmail.com>
2020-12-19 22:35:06 +0100

    slide statement and consolidate conditionals preparing for replace conditional with polymorphism

--- src/Shop.js
+++ src/Shop.js
@@ -25,14 +25,11 @@ export default class Shop {
         const shouldDegradeQuality = hasQuality(item.quality) && !isSulfuras && !isAgedBried && !isConcertBackstage
         const hasPassedSellInDay = (sellIn) => sellIn <= 0;
 
-        if (shouldDegradeQuality) {
+        if (shouldDegradeQuality && !hasPassedSellInDay(item.sellIn)) {
           item.quality = item.quality - 1;
-          if (hasPassedSellInDay(item.sellIn)) {
-            item.quality--;
-          }
-        }
-
-        if (isConcertBackstage) {
+        } else if (shouldDegradeQuality && hasPassedSellInDay(item.sellIn)) {
+          item.quality = item.quality - 2;
+        } else if (isConcertBackstage) {
           if (item.sellIn > 10 && !isMaxQuality(item.quality)) {
             item.quality = item.quality +1
           }
@@ -45,18 +42,17 @@ export default class Shop {
           if (item.sellIn < 0) {
             item.quality = 0;
           }
+        } else if (isAgedBried){
+          if (!isMaxQuality(item.quality)){
+            item.quality = item.quality + 1;
+          }
+          if (hasPassedSellInDay(item.sellIn) && !isMaxQuality(item.quality)) {
+            item.quality = item.quality + 1;
+          }
         }
 
         if (!isSulfuras) {
           item.sellIn = item.sellIn - 1;
-        }
-
-        if (isAgedBried && !isMaxQuality(item.quality)){
-          item.quality = item.quality + 1;
-          if (hasPassedSellInDay(item.sellIn)) {
-            item.quality = item.quality + 1;
-
-          }
         }
 
         return item
