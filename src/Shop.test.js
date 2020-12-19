376b0a952fce24d376ddbcac0dcbeebcc8dee4e5
Pedro La Rosa <pedrolarosad@gmail.com>
2020-12-19 17:14:42 +0100

    testing aged brie item

--- src/Shop.test.js
+++ src/Shop.test.js
@@ -1,6 +1,8 @@
 import Shop from "./Shop";
 import Item from "./Item";
 
+const agedBrieName = "Aged Brie"
+
 describe("Testing Shop class", () => {
   describe("Testing a regular item", () => {
     const regularItemName = "regular item";
@@ -36,4 +38,31 @@ describe("Testing Shop class", () => {
       expect(item).toEqual(new Item(regularItemName, -1, 0));
     });
   });
+
+  describe(`Testing ${agedBrieName} item`, () => {
+    it("should increase the quality by one every day", () => {
+      const item = new Item(agedBrieName, 10, 10);
+
+      new Shop([item]).updateQuality();
+
+      expect(item).toEqual(new Item(agedBrieName, 9, 11));
+    })
+
+    it("should quality increase twice as fast when the sellIn has passed", () => {
+      const item = new Item(agedBrieName, -5, 10);
+
+      new Shop([item]).updateQuality();
+
+      expect(item).toEqual(new Item(agedBrieName, -6, 12));
+    })
+
+    it("should not increase the quality of the item more than 50", () => {
+      const item = new Item(agedBrieName, -5, 50);
+
+      new Shop([item]).updateQuality();
+      new Shop([item]).updateQuality();
+
+      expect(item).toEqual(new Item(agedBrieName, -7, 50));
+    })
+  })
 })
